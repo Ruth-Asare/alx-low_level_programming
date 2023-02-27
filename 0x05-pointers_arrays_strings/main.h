97696aa63e32ef122c6fa83232f4474226657cb7
Ruth-Asare <asareruth159@gmail.com>
2023-02-27 09:54:14 -0800

    Write a function that takes a pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,7 @@
-#include <stdio.h>
-#include "main.h"
-
-
 #ifndef main.h
-#define main.h
+#define "main.h"
+#include <stdio.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
